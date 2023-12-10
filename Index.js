e1f50d2f4ec9a0948ba7e8e10cee45fc327231ae
Ajinath <ajinath2151@gmail.com>
2023-12-10 14:59:11 +0530

    use of import statemnt in nodejs, export obj from second.js and import in index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,5 @@
-console.log("hello world!");
+// import scond.js file object data in this indexedDB.js file using import
+
+const ajinathData = require('./second')
+
+console.log("hello world..!" , ajinathData); //ajinathData accessed from second.js file
---
+++ second.js
@@ -0,0 +1,9 @@
+
+// send second.js file object data to index.js
+
+ajinathObject = {
+    name: "Ajinath",
+    age: 28,
+    developer:'true',
+}
+module.exports =  ajinathObject;
