3a5e1b188f4cf9e720133bcbcc56de2b1da55790
Ajinath <ajinath2151@gmail.com>
2023-12-11 15:51:41 +0530

    ES-6 / ecma script modules and it's import type exmple

---
+++ moduleFourth.mjs
@@ -0,0 +1,10 @@
+
+// ES-6 module example
+export function simple() {
+    console.log("simple function of ES-6 module");
+}
+export function simple2() {
+    console.log("simple-2 function of ES-6 module");
+}
+
+//changes 1) do not use module.export use export at the top
---
+++ moduleThird.js
@@ -0,0 +1,15 @@
+
+// ES-6 module example
+
+import { simple, simple2 } from "./moduleFourth.mjs";
+simple();
+simple2();
+
+
+
+// for multiple import
+// import  * as a  from "./moduleFourth.mjs";
+// console.log(a.simple());
+// console.log(a.simple2());
+
+//changes 1) do not use const =  require use import
